591ac4d4d2cb4f2e5c500d8e92e44ef8cf69c29d
sal400 <flarebd@gmail.com>
2024-03-12 05:13:24 +0600

    /properties >> Create operation added for property listing

--- index.js
+++ index.js
@@ -13,37 +13,48 @@ const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // await client.connect();
-	const database = client.db('swissrealty_db')
-    const properties = database.collection('properties')
-    
+    try {
+        // await client.connect();
+        const database = client.db('swissrealty_db')
+        const properties = database.collection('properties')
 
 
+        /* PROPERTIES START */
+        //properties >> Create
+        app.post('/properties', async (req, res) => {
+            const property = req.body
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+            const result = await properties.insertOne(property)
+            res.send(result)
+        })
+
+
+        /* PROPERTIES END */
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Welcome to express-server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`express-server is running on ${port}`)
 })
